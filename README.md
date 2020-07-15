96a165b28277c0f0c9ca4d883d08e19a56c9dbd0
Jemima Olanrewaju <jemimaolanrewaju@gmail.com>
2020-07-15 03:36:23 +0100

    comment

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Olanrewaju_19249067_AS2_part1
+
+![validation of html](images/htmlprob1.jpg)
---
+++ main.js
@@ -0,0 +1,11 @@
+/* SCRIPTS.JS */
+/* the picure is called using the ID it is called from index.html. code is gotten from class and also from
+https://codepen.io/lizcoultersmith/pen/jOPKWxe*/
+function changePic() {
+    document.getElementById("profile-pic").src = "images/profile_img.jpg";
+  }
+  
+function normalPic() {
+    document.getElementById("profile-pic").src = "images/change.jpg";
+  }
+ 
